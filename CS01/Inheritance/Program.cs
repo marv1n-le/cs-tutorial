4ca31794e6bf8244e154556493a6565ec3054887
duc-anh-le <ducanhle.iter@gmail.com>
2024-12-16 14:38:02 +0700

    Add CS01: Inheritance

---
+++ CS01/Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Inheritance
+{
+    class Animal
+    {
+        public int Legs { get; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }   
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
