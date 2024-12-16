9fa7949d51ca35f1ac0660c16b5b33ee9b037186
duc-anh-le <ducanhle.iter@gmail.com>
2024-12-16 20:06:10 +0700

    Add CS04: Virtual Method

---
+++ CS04/VirtualMethod/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VirtualMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
