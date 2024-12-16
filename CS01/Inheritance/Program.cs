aef9c5527d58ec6ed39a50056b22511294dee234
duc-anh-le <ducanhle.iter@gmail.com>
2024-12-16 15:36:19 +0700

    Update CS01: Inheritance

--- CS01/Inheritance/Program.cs
+++ CS01/Inheritance/Program.cs
@@ -1,14 +1,55 @@
 ﻿namespace Inheritance
 {
-    class Animal
+     class Animal
     {
-        public int Legs { get; set; }
+        public Animal()
+        {
+            Console.WriteLine("Animal constructor");
+        }
 
+        public Animal(string ab)
+        {
+            Console.WriteLine("Animal constructor(2) " + ab);
+        }
+        public int Legs { get; set; }
+        public float Weight { get; set; }
+        public void ShowLegs()
+        {
+            Console.WriteLine("Legs: " + Legs);
+        }
+    }
+
+    //sealed: niem phong lop, khong cho ke thua
+
+    class Cat : Animal
+    {
+        public string Food;
+
+        public Cat(string s) : base(s) //muon goi phuong thuc cua lop cha thi phai co tu khoa base, neu khong co thi mac dinh goi phuong thuc khong tham so
+        {
+            this.Legs = 4;
+            this.Food = "Fish";
+            Console.WriteLine("Cat constructor");
+        }
         public void Eat()
         {
             Console.WriteLine("Eating...");
         }
-    }   
+        public new void ShowLegs()
+        {
+            Console.WriteLine("Cat's legs: " + Legs);
+        }
+        public void ShowInfo()
+        {
+            base.ShowLegs(); // goi phuong thuc cua lop cha
+            ShowLegs();
+        }
+    }
+
+    class A { }
+    class B : A { }
+    class C : B { }
+    // a co the new B, nhung b khong the new A -> con khong the new cha chi co the cha new duoc con hoac chinh no
 
 
 
@@ -16,7 +57,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Cat cat = new Cat("toi la con meo");
+            A a = new B();
+            B b = new C();
+            //C c = new A(): 
         }
     }
 }
