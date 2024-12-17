00c6f2a52008d8aeddce778ad72bf42d28b44583
duc-anh-le <ducanhle.iter@gmail.com>
2024-12-17 18:53:39 +0700

    Add CS08: Exception

---
+++ CS08/Exception/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exception_
+{
+    public class NameEmptyException : Exception
+    {
+        public NameEmptyException() : base("Ten phai khac rong")
+        {
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+
+            //var c = a / b;
+            try
+            {
+                var c = a / b;
+                int[] arr = {1 , 2};
+                var x = arr[7];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Source);
+                Console.WriteLine(ex.GetType().Name);
+            }
+
+
+            static void Register(string name, string age)
+            {
+                Console.WriteLine("Registering...");
+                if(string.IsNullOrEmpty(name))
+                {
+                    Exception ex = new Exception("Name is required");
+                    throw new NameEmptyException();
+                }
+            }
+
+            try
+            {
+                Register("", "20");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
